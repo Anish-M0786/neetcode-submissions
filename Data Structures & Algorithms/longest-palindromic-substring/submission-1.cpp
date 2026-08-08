bd396bb3416c0422e65f969d598731f8e2f9bfbf
Anish M <anishmb0786@gmail.com>
2026-08-08 16:10:34 +0530

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if(n==1) return s;
+        int start = 0 , end = 1;
+        for(int i=0;i<n;i++){
+            int left = i , right = i;
+            while(left>=0 && right<n && s[left]==s[right]){
+                if(right-left+1>end){
+                    end = right-left+1;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+             left = i , right = i+1;
+            while(left>=0 && right<n && s[left]==s[right]){
+                if(right-left+1>end){
+                    end = right-left+1;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substr(start,end);
+    }
+};
---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        if(s.length()==0) return 0;
+       int count = 0;
+       for(int i =0;i<s.length();i++){
+
+          int left = i , right = i;
+          while(left>=0 && right<s.length() && s[left]==s[right]){
+            count++;
+          
+          left--;
+          right++;
+          }
+          left = i , right = i+1;
+          while(left>=0 && right<s.length() && s[left]==s[right]){
+            count++;
+            left--;
+            right++;
+          }
+       }
+       
+
+       return count;
+
+    }
+};
