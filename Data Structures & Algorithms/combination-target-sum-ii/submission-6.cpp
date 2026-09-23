8501e9be3b40e36c66d48a7c1bd0d0b60458ce27
Anish M <anishmb0786@gmail.com>
2026-09-23 18:04:04 +0530

    Add: combination-target-sum-ii - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+vector<vector<int>>ans;
+vector<int>logic;
+void backtrack(vector<int>&candidates,int target,int index){
+    if(target==0){
+        ans.push_back(logic);
+    }
+    if(index==candidates.size()) return;
+    for(int i=index;i<candidates.size();i++){
+        if(i>index && candidates[i]==candidates[i-1]){
+            continue;
+        }else if(candidates[i]>target){
+            continue;
+        }else{
+            logic.push_back(candidates[i]);
+        }
+        backtrack(candidates,target-candidates[i],i+1);
+        logic.pop_back();
+    }
+}
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        backtrack(candidates,target,0);
+        return ans;
+
+        
+    }
+};
